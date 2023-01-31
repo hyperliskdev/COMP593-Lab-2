a6d4453581c0c808c15ec6105e1f58a3f3c9903b
Carter Tomlenovich <cartertomlenovich@gmail.com>
2023-01-31 11:21:19 -0500

    finished script with bonuses

--- script.py
+++ script.py
@@ -1,3 +1,5 @@
+
+
 def main():
     info_dict = {
         "full_name": "Carter Tomlenovich",
@@ -8,18 +10,27 @@ def main():
             "BACON"
         ],
         "movies": [
-            { "title": "Terminator 3: Rise of the Machines", "genre": "sci-fi"},
-            { "title": "Spiderman 2", "genre": "action"}
+            { 
+                "title": "Terminator 3: Rise of the Machines", 
+                "genre": "sci-fi"
+            },
+            { 
+                "title": "Spiderman 2",
+                "genre": "action"
+            }
         ]
     }
 
-    info_dict["movies"].append( { "title": "Interstellar", "genre": "sci-fi" })
+    info_dict["movies"].append( { "title": "Split", "genre": "psychological" })
+
+    print_student_info(info_dict)
 
     print_pizza_toppings(info_dict)
     add_pizza_topping(info_dict, ["cheese", "green peppers"])
     print_pizza_toppings(info_dict)
 
-    print(info_dict)
+    print_movie_genres(info_dict)
+    print_movie_titles(info_dict)
     
     return
 
@@ -43,10 +54,40 @@ def add_pizza_topping(dict, new_toppings):
 
 
 def print_pizza_toppings(dict):
-    print("My favorite pizza toppings are: \n")
+    print("\nMy favorite pizza toppings are: \n")
 
     for topping in dict["pizza_toppings"]:
         print("- {0}".format(topping))
+
+def print_movie_genres(dict):
+    movies = dict["movies"]
+    genres = []
+
+    for movie in movies:
+        genres.append(movie["genre"])
+
+    print("I like to watch ", end="")
+    for i, g in enumerate(genres):
+        if g == genres[-1]:
+            print("and {0} movies.".format(g))
+        else:
+            print("{0}, ".format(g), end="")
+
+
+def print_movie_titles(dict):
+    movies = dict["movies"]
+    titles = []
+
+    for movie in movies:
+        titles.append(movie["title"])
+
+    print("\nSome of my favorite movies are ", end="")
+    for i, t in enumerate(titles):
+        if t == titles[-1]:
+            print("and {0}!".format(t))
+        else:
+            print("{0}, ".format(t), end="")
+
 
 if __name__ == "__main__":
     main()
